dc7ec38fc5251cfc0191a7a4a86b2134d969230e
Altix <altix.cs@gmail.cim>
2022-09-04 23:59:57 +0200

    Commit

--- EnglishLearning/settings.py
+++ EnglishLearning/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'EnglishLearning/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'C:/Users/Branfi/PycharmProjects/EnglishLearning/static'
 
 
 
